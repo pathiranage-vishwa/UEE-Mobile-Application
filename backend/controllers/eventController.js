79a834cee8b0137b6118a1b187cf25c14ad86df0
Piumika Saranga <IT20260460@my.sliit.lk>
2022-10-30 19:56:07 +0530

    update createevent Controller

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -27,10 +27,23 @@ const createEvent = async (req, res) => {
     });
     res.status(201).json({ event });
   } catch (err) {
-    console.log(err);
+    res.status(400).json({ message: err.message });
   }
 };
 
+//get all events
+const getAllEvents = async (req, res) => {
+  try {
+    const events = await Event.find({});
+    res.status(200).json({ events });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//get event by id
+
 module.exports = {
   createEvent,
+  getAllEvents,
 };
