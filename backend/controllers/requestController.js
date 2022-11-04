107410725325e1bd22c44edd2d20caaad9bd8731
Ravindu Yasith <IT20236014@my.sliit.lk>
2022-11-05 00:46:04 +0530

    requests backend

---
+++ backend/controllers/requestController.js
@@ -0,0 +1,99 @@
+const Request = require("../models/eventModel");
+
+//create event
+const createRequest = async (req, res) => {
+  try {
+    const {
+      title,
+      category,
+      location,
+      description,
+      image,
+    } = req.body;
+    const request = await Request.create({
+        title,
+        category,
+        location,
+        description,
+        image,
+    });
+    res.status(201).json({ request });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//get all events
+const getAllRequests = async (req, res) => {
+  try {
+    const requests = await Request.find({});
+    res.status(200).json({ requests });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//get event by id
+const getRequestById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await Request.findById(id);
+    if (request) {
+      return res.status(200).json({ request });
+    }
+    res.status(404).send("Request not found");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//update event
+const updateRequest = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      title,
+      category,
+      location,
+      description,
+      image,
+    } = req.body;
+    const request = await Request.findByIdAndUpdate(
+      id,
+      {
+        title,
+        category,
+        location,
+        description,
+        image,
+      },
+      { new: true }
+    );
+    res.status(200).json({ request });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//delete event
+const deleteRequest = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await Request.findByIdAndDelete(id);
+    if (request) {
+      return res.status(200).json({ message: "Request deleted" });
+    }
+    res.status(404).send("Request not found");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+    createRequest,
+    getAllRequests,
+    getRequestById,
+    updateRequest,
+    deleteRequest,
+  };
+  
---
+++ backend/models/requestModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const RequestSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const Request = mongoose.model("requests", RequestSchema);
+module.exports = Request;
